98f82473f3c48417c28502a740985739c03ade9b
Rashnain <51191602+Rashnain@users.noreply.github.com>
2025-04-06 19:11:24 +0200

    interface: ajout sélecteur de mode et taille imagette

--- TP_Mosaique/interface/main.py
+++ TP_Mosaique/interface/main.py
@@ -5,9 +5,9 @@ from tkinter import filedialog, Label
 root = tk.Tk()
 root.title("Projet Image")
 
-mode = "0"
+modes = ["Moyenne", "Distance d'histogramme"]
+imagette_sizes = ["4", "8", "16", "32", "64", "128"]
 image_path = ""
-imagette_size = "32"
 
 def choose_image():
     global image_path
@@ -18,22 +18,44 @@ def choose_image():
     )
     if image_path:
         file_label.config(text=f"Image choisie : {image_path}")
-        choose_button.destroy()
         transform_button.pack(pady=20)
     else:
         file_label.config(text="Veuillez choisir une image à transformer en mosaïque")
 
 def transform():
-    transform_button.destroy()
     # TODO implémenter les arguments du côté C++
+    if dropdown_var_mode.get() == modes[0]:
+        mode = "0"
+    else:
+        mode = "1"
+    imagette_size = dropdown_var_size.get()
+    print(f"../base_code_mosaique {mode} {image_path} {imagette_size}")
     os.system(f"../base_code_mosaique {mode} {image_path} {imagette_size}")
 
 file_label = Label(root, text="Veuillez choisir une image à transformer en mosaïque", wraplength=350)
 file_label.pack(pady=20)
 file_label.pack(padx=20)
 
-choose_button = tk.Button(root, text="Choisir l'image", command=choose_image)
-choose_button.pack(pady=20)
+choose_button = tk.Button(root, text="Choisir une image", command=choose_image)
+choose_button.pack()
+
+mode_label = Label(root, text="Veuillez choisir un mode de transformation", wraplength=350)
+mode_label.pack(pady=20)
+mode_label.pack(padx=20)
+
+dropdown_var_mode = tk.StringVar(root)
+dropdown_var_mode.set(modes[0])
+dropdown_menu_mode = tk.OptionMenu(root, dropdown_var_mode, *modes)
+dropdown_menu_mode.pack()
+
+size_label = Label(root, text="Veuillez choisir la taille des imagettes", wraplength=350)
+size_label.pack(pady=20)
+size_label.pack(padx=20)
+
+dropdown_var_size = tk.StringVar(root)
+dropdown_var_size.set(imagette_sizes[3])
+dropdown_menu_size = tk.OptionMenu(root, dropdown_var_size, *imagette_sizes)
+dropdown_menu_size.pack(pady=10)
 
 transform_button = tk.Button(root, text="Transformer l'image", command=transform)
 
